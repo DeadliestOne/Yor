e55001d4a4427a71d4bd7afb2f71b0196a21de61
DeadliestOne <uncountableaura@gmail.com>
2024-09-13 16:23:22 +0530

    Update couples.py

--- PURVIMUSIC/plugins/tools/couples.py
+++ PURVIMUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from PURVIMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="|| ˹𖤍 𝗔𝗟𝗣𝗛𝗔 𖤍˼ ||",
-            url=f"https://t.me/PURVI_UPDATES",
+            text="ᴅᴇᴠᴇʟᴏᴘᴇʀ 🤭",
+            url=f"https://t.me/Ind_Administrator",
         ),
     ],
 ]
